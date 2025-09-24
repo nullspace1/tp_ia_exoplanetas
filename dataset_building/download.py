9879679c134fb5850a4be6732af4ca79e39fef4c
Lautaro Moyano <36833412+nullspace1@users.noreply.github.com>
2025-09-23 22:15:17 -0300

    Managed to process full kepler dataset for negative samples.

--- dataset_building/download.py
+++ dataset_building/download.py
@@ -5,8 +5,10 @@ import pandas as pd
 import zipfile
 import lightkurve as lk
 
+config : dict = json.load(open("config.json"))
+
 def download_main_dataset():
-    config : dict = json.load(open("config.json"))
+
     
     os.environ["KAGGLE_USERNAME"] = config["username"]
     os.environ["KAGGLE_KEY"] = config["key"]
@@ -20,6 +22,13 @@ def download_main_dataset():
     with zipfile.ZipFile(zip_path, 'r') as zip_ref:
         with zip_ref.open("cumulative.csv") as f:
             df = pd.read_csv(f)
+    
+    return df
+
+def download_secondary_dataset():
+    
+    path = config["secondary_dataset_path"]
+    df = pd.read_csv(path, sep="|", header=0)
     
     return df
 
@@ -42,9 +51,26 @@ def download_processed_light_curve(kepler_id : str):
 
 if __name__ == "__main__":
     
-    df :  pd.DataFrame = download_main_dataset()
-
-    for kepler_id in df["kepid"]:
+    exoplanet_df :  pd.DataFrame = download_main_dataset()
+    no_exoplanet_df : pd.DataFrame = download_secondary_dataset()
+    
+    no_exoplanet_df = no_exoplanet_df[no_exoplanet_df["nconfp"] == 0]
+    
+    exoplanet_lookup_table = exoplanet_df[["kepid", "koi_steff", "koi_srad", "ra", "dec", "koi_kepmag","koi_time0bk","koi_impact","koi_period","koi_duration", "koi_disposition"]]
+    exoplanet_lookup_table.to_csv("data/candidates_lookup_table.csv", index=False)
+    
+    
+    no_exoplanet_lookup_table = no_exoplanet_df[["kepid","teff","radius","ra","dec","kmag"]]
+    
+    no_exoplanet_lookup_table.loc[:, "koi_time0bk"] = 0
+    no_exoplanet_lookup_table.loc[:, "koi_impact"] = 0
+    no_exoplanet_lookup_table.loc[:, "koi_period"] = 0
+    no_exoplanet_lookup_table.loc[:, "koi_duration"] = 0
+    no_exoplanet_lookup_table.loc[:, "koi_disposition"] = "NEGATIVE"
+    
+    no_exoplanet_lookup_table.to_csv("data/no_candidates_lookup_table.csv", index=False)
+    
+    for kepler_id in exoplanet_df["kepid"].unique():
         results = download_raw_light_curve(kepler_id)
         if results is not None and isinstance(results, lk.LightCurveCollection) and len(results) > 0:
             lc : lk.lightcurve.LightCurve = results.stitch()
@@ -52,4 +78,5 @@ if __name__ == "__main__":
             lc_table = lc.to_pandas()
             lc_table["time"] = lc.time.value
             lc_table = lc_table.filter(['time','flux','flux_err'])
-            lc_table.to_csv(f"data/{kepler_id}.csv", index=False)
+            lc_table.to_csv(f"data/light_curves/{kepler_id}.csv", index=False)
+            
